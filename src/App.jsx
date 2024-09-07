9576349f18fe12d445612875f3d46ddec66a3178
adiya-deokar <adiyadeokar80@gmail.com>
2024-09-07 17:31:51 +0530

    desktop header created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import ButtonGradient from "./assets/svg/ButtonGradient"
-import Button from "./components/Button";
+
+import Header from "./components/Header";
 
 const App = () => {
 
@@ -7,13 +8,9 @@ const App = () => {
   return (
 
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10"  >
-          something
-        </Button>
+        <Header />
       </div>
       <ButtonGradient />
 
