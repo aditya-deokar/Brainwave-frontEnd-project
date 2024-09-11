04c7960552935467fea4d1ab25e6541d14a4108b
adiya-deokar <adiyadeokar80@gmail.com>
2024-09-11 22:23:40 +0530

    add generating component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,7 @@ import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
 import { heroIcons } from "../constants";
 import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
+import Generating from "./Generating";
 
 const Hero = () => {
   const parallaxRef=useRef(null)
@@ -41,12 +42,14 @@ const Hero = () => {
               <div className='relative bg-n-8 rounded-[1rem]'>
                   <div className='h-[1.4rem] bg-n-10 rounded-t-[0.9rem]'></div>
 
-                  <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]"> 
+                  <div className=" aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]"> 
                     <img src={robot} alt="robot" 
                       className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[15%]"
                       width={1024}
                       height={490}
                     />
+
+                    <Generating className="absolute left-4 right-4 bottum-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2 "/>
 
                     <ScrollParallax isAbsolutelyPositioned>
                       <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
