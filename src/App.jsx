74ab02d37337b6a5c7b9878522e6fb45b8715fb4
adiya-deokar <adiyadeokar80@gmail.com>
2024-09-16 12:41:10 +0530

    roadmap page added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Collaboration from "./components/Collaboration";
 
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
 
 const App = () => {
@@ -19,6 +20,7 @@ const App = () => {
         <Benifites />
         <Collaboration/>
         <Services/>
+        <Roadmap/>
 
       </div>
       <ButtonGradient />
